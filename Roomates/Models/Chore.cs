71254e4ed9ce3e49330c903676fd7ddb1638c95f
Sam Carbine <scarbine@me.com>
2021-09-10 11:08:13 -0500

    first commit

---
+++ Roomates/Models/Chore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roomates.Models
+{
+    class Chore
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+
+}
+}
---
+++ Roomates/Models/Roomate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roomates.Models
+{
+    class Roomate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int RentPortion { get; set; }
+        public DateTime MovedInDate { get; set; }
+        public Room Room { get; set; }
+
+
+    }
+}
